4bc9089cbfe6ce24cb2ff7cda24e7d81ac9b6d8b
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-08-09 08:52:25 -0600

    added link to calendar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,7 @@
 import CustomImage from "./CustomImage";
 
+import { Link } from "react-router-dom";
+
 export default function HeroSection() {
     const images = [
         "/images/gallery/img_1.jpg",
@@ -17,7 +19,9 @@ export default function HeroSection() {
       <div className="col typography">
         <h1 className="title"> What started my nanny journey?</h1>
         <p className="info"> As a recent graduate of culinary school I didn't feel like immediatly settling down and finding a job I wanted to explore and see the world so I decided to start on a few small au pair websites and see if I could be a nanny. Fast forward almost 15 years and I have created my own thriving business.</p>
-        <button className="btn">Explore Now</button>
+        <button className="btn">
+          <Link to="/calendar">Book a Session</Link>
+        </button>
         </div>
       <div className=" col gallery">
         {images.map((src, index) =>(
